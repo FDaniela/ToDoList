cc75c836474f60037ed3af21b4b4fe503a2f98a3
Daniela <102395421+FDaniela@users.noreply.github.com>
2023-07-09 22:56:31 -0300

    Add Side Bar

--- Front/src/app/app.module.ts
+++ Front/src/app/app.module.ts
@@ -5,11 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {DragDropModule} from '@angular/cdk/drag-drop';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { BoardComponent } from './components/board/board.component';
+import {MatSidenavModule} from '@angular/material/sidenav'; 
+import { MatIconModule } from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 import { StatusColumnComponent } from './components/status-column/status-column.component';
 import { TaskBlockComponent } from './components/task-block/task-block.component';
 import { TodoBlockComponent } from './components/todo-block/todo-block.component';
-import { BoardComponent } from './components/board/board.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+ 
 
 @NgModule({
   declarations: [
@@ -17,11 +23,16 @@ import { BoardComponent } from './components/board/board.component';
     StatusColumnComponent,
     TaskBlockComponent,
     TodoBlockComponent,
-    BoardComponent
+    BoardComponent,
+    SideBarComponent
   ],
   imports: [
     BrowserModule,
+    MatToolbarModule,
+    MatListModule,
     DragDropModule,
+    MatIconModule,
+    MatSidenavModule,
     BrowserAnimationsModule,
     AppRoutingModule
   ],
---
+++ Front/src/app/components/side-bar/side-bar.component.ts
@@ -0,0 +1,21 @@
+import { Component, inject } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
+
+
+@Component({
+  selector: 'app-side-bar',
+  templateUrl: './side-bar.component.html',
+  styleUrls: ['./side-bar.component.css']
+})
+export class SideBarComponent {
+  private breakpointObserver = inject(BreakpointObserver);
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+}
